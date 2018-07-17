d4fcd023c16d105a7d515aece98bb57bd7c0feb8
Shobhit <shobhit55@gmail.com>
2018-07-17 10:17:19 +0800

    Windows build

--- src/util.h
+++ src/util.h
@@ -1,14 +1,15 @@
 // Copyright 2018 Filegear - All Rights Reserved
 
 #pragma once
-
-#include <arpa/inet.h>
-
 #define return_false_if(condition) \
   if (condition) { return false; }
 
 #define return_false_if_msg(condition, ...) \
   if (condition) { printf(__VA_ARGS__); return false; }
 
+#ifndef _WIN32
+#include <arpa/inet.h>
+
 uint64_t htonll(uint64_t val);
 uint64_t ntohll(uint64_t val);
+#endif
