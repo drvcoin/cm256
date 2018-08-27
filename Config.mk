8271e824ffda46f1d88996c7a68814a54347916f
Jason Zhu <jasonzhu@filegear.com>
2018-08-27 13:58:50 +0800

    Add git submodules for library projects

--- Config.mk
+++ Config.mk
@@ -1,16 +1,16 @@
-# 
+#
 # Copyright (c) 2018 Drive Foundation
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,19 +18,32 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-# 
+#
 
-ROOT = $(realpath ..)
+ifeq ($(ROOT),)
+$(error "Please define ROOT variable to point to the repository's root folder")
+endif
 
-TARGETLIB = cm256
+OUTDIR = $(ROOT)/out
+BINDIR = $(OUTDIR)/bin
+CFGDIR = $(OUTDIR)/cfg
+PKGDIR = $(OUTDIR)/package
 
-include $(ROOT)/Config.mk
+ifneq ($(TARGETLIB),)
+TARGETNAME = $(TARGETLIB)
+TARGETFILENAME = $(TARGETNAME).a
+else
+ifneq ($(TARGETSO),)
+TARGETNAME = $(TARGETSO)
+TARGETFILENAME = $(TARGETNAME).so
+else
+TARGETNAME = $(TARGETEXE)
+TARGETFILENAME = $(TARGETNAME)
+endif
+endif
 
-SRCS = \
-  cm256.cpp \
-  gf256.cpp \
-  util.cpp
+OBJDIR = $(OUTDIR)/obj/$(TARGETNAME)
+INCDIR = $(OUTDIR)/inc
+LIBDIR = $(BLDDIR)/lib
 
-CCFLAGS += -std=c++11 -msse4.1
-
-include $(ROOT)/Cpp.mk
+BUILD_ROOT = $(ROOT)/build
--- Makefile
+++ Makefile
@@ -1,16 +1,18 @@
-# 
-# Copyright (c) 2018 Drive Foundation
-# 
+#
+# MIT License
+#
+# Copyright (c) 2018 drvcoin
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,19 +20,21 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-# 
+#
+# =============================================================================
+#
 
-ROOT = $(realpath ..)
-
-TARGETLIB = cm256
-
+ROOT := $(realpath .)
 include $(ROOT)/Config.mk
 
-SRCS = \
-  cm256.cpp \
-  gf256.cpp \
-  util.cpp
+DIRS = \
+	src
 
-CCFLAGS += -std=c++11 -msse4.1
+.PHONY: all clean $(DIRS)
 
-include $(ROOT)/Cpp.mk
+all: $(DIRS)
+
+clean: $(DIRS)
+
+$(DIRS):
+	@$(MAKE) -C $@ $(MAKECMDGOALS) $$([ -f $@/Makefile.mk ] && echo -n "-f Makefile.mk")
